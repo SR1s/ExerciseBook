097e46bb7a7a7b6defc803da5203187512ac9937
SR1s <seemsnevermind@gmail.com>
2014-05-01 00:28:07 +0800

    implement mylists:filter

--- 14-04-30/mylists.erl
+++ 14-04-30/mylists.erl
@@ -1,18 +1,13 @@
 -module(mylists).
--export([map/2]).
+-export([map/2, map/2, filter/2]).
 
 sum([H|T])    -> H + sum(T);
 sum([])       -> 0.
 
-% todo..
 % map(X,[H|T])  -> [X(H)|map(X,T)];
 % map(X,[])     -> [].
 
-% filter(X,[H|T])-> (if
-%                       X(H) -> [X(H)|filter(X,T)];
-%                   filter(X,T)
-%                   end);
-% filter(X,[])   -> [].
+filter(Function, List)-> [ Element || Element <- List, Function(Element)].
 
 % a new map implemention which is more simple
 map(Fun,List)  -> [Fun(E) || E <- List].
